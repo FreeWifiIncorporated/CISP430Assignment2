804d3ef068f13b4dd656a9952d74a6e629e244a7
w1568289 <w1568289@outlook.com>
2017-09-20 17:22:19 -0700

    dsadas

---
+++ CISP430V4A2/CISP430V4A2/sequence2.cpp
@@ -0,0 +1,60 @@
+// Lorin Zhang
+// sequence.cpp is the implementation file for sequence.h
+
+#include <cstdlib>  // Provides size_t
+#include "sequence2.h"
+
+using namespace std;
+
+namespace CISP430_A2
+{
+	sequence::sequence(size_type entry)
+	{
+		//allocate a chunk of memory on the heap
+		data = new value_type[entry];
+
+		//initialize our values
+		used = 0;
+		capacity = entry;
+		current_index = 0;
+	}
+	sequence::sequence(const sequence & entry)
+	{
+	}
+	void sequence::start()
+	{
+	}
+	void sequence::advance()
+	{
+	}
+	void sequence::insert(const value_type & entry)
+	{
+	}
+	void sequence::attach(const value_type & entry)
+	{
+	}
+	void sequence::remove_current()
+	{
+	}
+	void sequence::resize(size_type)
+	{
+	}
+	void sequence::operator=(const sequence &)
+	{
+	}
+	size_t sequence::size() const
+	{
+		return size_type();
+	}
+	bool sequence::is_item() const
+	{
+		return false;
+	}
+	double sequence::current() const
+	{
+		return value_type();
+	}
+	sequence::~sequence()
+	{
+	}
+}
