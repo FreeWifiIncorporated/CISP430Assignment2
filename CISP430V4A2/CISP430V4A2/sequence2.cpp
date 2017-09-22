be5e182dc4c6cd8557e26209d2f9cbbff82564d4
w1568289 <w1568289@outlook.com>
2017-09-21 23:14:50 -0700

    attach edited

--- CISP430V4A2/CISP430V4A2/sequence2.cpp
+++ CISP430V4A2/CISP430V4A2/sequence2.cpp
@@ -66,15 +66,15 @@ namespace CISP430_A2
 	}
 	void sequence::attach(const value_type & entry)
 	{
-		if (size() > CAPACITY) // 
+		if (size() > capacity) // 
 		{
-			resize(size_t(capacity += CAPACITY * .10); // if so increase the capacity by 10%
+			resize(size_t(capacity * 1.1); // if so increase the capacity by 10%
 		}
 
 		if (is_item())
 		{
 			current_index++; // to insert after current item
-			for (int i = used; data[i] > current_index; --i)
+			for (size_type i = used; data[i] > current_index; --i)
 			{
 				data[i] = data[i - 1]; // insert new copy
 			}
