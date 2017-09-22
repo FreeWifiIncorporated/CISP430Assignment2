a31c3d9c8c1e133b2efc41acebe4941214a40de7
NathanYarrison <31556575+NathanYarrison@users.noreply.github.com>
2017-09-21 20:31:00 -0700

    Insert and remove_current functions have been added.

--- CISP430V4A2/CISP430V4A2/sequence2.cpp
+++ CISP430V4A2/CISP430V4A2/sequence2.cpp
@@ -8,6 +8,7 @@
 
 #include "sequence2.h"
 #include <cstdlib>
+#include <cassert>
 
 namespace CISP430_A2
 {
@@ -48,8 +49,24 @@ namespace CISP430_A2
 	{
 		//Adds a new item to the front of the sequence.
 
+		//Check if in the right spot to add a new item.
+		size_type i; //Will be used to check against current_index to see if in the right.
+
+		//Check if size is greater than capacity. If it is, then increase the capacity by 10%
+		if (size() > CAPACITY)
+		{
+			capacity += CAPACITY * .10; //Increases the capacity of the sequence by 10% of CAPACITY.
+			//TBH, not even sure why this is needed.
+		}
+
+		if (!is_item())
+		{
+			//This checks if there is an item and if there isn't. sets current_index to 0 so that the new item is added at the front of the sequence.
+			current_index = 0;
+		}
+
 		//Shift original items to the right one to make room for the new item.
-		for (int i = used; data[i] > 0; --i)
+		for ( i = used; data[i] > 0; --i)
 		{
 			data[i] = data[i - 1];
 		}
@@ -64,6 +81,21 @@ namespace CISP430_A2
 
 	void CISP430_A2::sequence::remove_current()
 	{
+		//Removes the current item from the index.
+		for (int i = current_index + 1; i < used; i++)
+		{
+			data[i - 1] = data[i]; //Takes an item in the sequence and moves it to the element before, removing the current item by shifting every item after over to the left
+		}
+
+		used--; //Decrements used by 1 because there is now one less item in the sequence.
+
+		//Check if reset of capacity is needed.
+		if ((used <= CAPACITY))
+		{
+			//If the size of the sequence is less than the CAPACITY, reset the sequences capacity.
+			//This clears up some memory. Not much, but it's still some memory freed up
+			capacity = CAPACITY;
+		}
 	}
 
 	void CISP430_A2::sequence::resize(size_type)
